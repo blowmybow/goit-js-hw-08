fee6c772cb3832c952137c990495b76b1fb4df26
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-04-01 11:15:46 +0300

    01-gallery and 02-video finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(STORAGE_KEY, e.seconds);
+  }, 1000)
+);
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .catch(function (error) {
+    console.error(error);
+  });
